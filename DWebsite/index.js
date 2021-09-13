aa53107cfd78636b5e6ad9b32ad585c65947389e
sethmartin2004 <71459713+sethmartin2004@users.noreply.github.com>
2021-09-13 10:00:56 -0400

    Added In Existing Code
    yes

---
+++ DWebsite/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.get('/', (req, res) => { //checks to see if what endpoint the user used, if nothing then say hello world
+    res.send('hello world')
+});
+
+app.listen(port, () => {
+    console.log(`ChatApp Listening at http://localhost:${port}`); // Server-side response in the console telling that the server is online
+})
+
+app.get('/yum', function (req, res) {
+    res.sendFile(path.join(__dirname + '/mukbang.html')); //what the end user sees
+})
+
+app.get('/secret', function(req, res) {
+  res.send('you found the secret market gasp emoji');
+})
