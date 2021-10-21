4b61cf5f823b134e78fb7800ce36ae24ada00e2b
sethmartin2004 <71459713+sethmartin2004@users.noreply.github.com>
2021-10-21 14:07:44 -0400

    new
    yes

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 //setup public folder
 app.use(express.static('./public'));
+app.use(express.urlencoded({extended: true}));
 
 var rawdata = fs.readFileSync('profiles.json');
 var profiles = JSON.parse(rawdata);
-rawdata = fs.readFileSync('comments.json');
-var commentFile = JSON.parse(rawdata);
+
 
 app.get('/',function (req, res) {
 res.render('pages/home')
@@ -36,7 +36,12 @@ app.get('/saimye',function (req, res) {
 res.render('pages/profile', profiles.saimye)
 });
 app.get('/feedback',function (req, res) {
-  res.render('pages/feedback.ejs')
+  rawdata = fs.readFileSync('comments.json');
+  let commentFile = JSON.parse(rawdata);
+  console.log(commentFile.comments);
+  res.render('pages/feedback.ejs', {
+    comments: commentFile.comments
+  })
 });
 app.post('/feedback',function (req, res) {
   let object = {
@@ -44,10 +49,15 @@ app.post('/feedback',function (req, res) {
     adjective: req.body.adjective
   }
   //check to make sure both varaiables have a value
-  if (object.name && object.adjective) {
-    object = JSON.stringify(object)
-    fs.writeFile('comments.json', object, 'utf8', function(){
-      res.send("Wrote to file")
+  if (object.name && object.adjective) {    
+    rawdata = fs.readFileSync('comments.json');
+    let commentFile = JSON.parse(rawdata);
+
+    commentFile.comments.push(object)
+    fs.writeFile('comments.json', JSON.stringify(commentFile), 'utf8', function(){
+      res.render('pages/feedback.ejs', {
+        comments: commentFile.comments
+      })
     })
   } else {
     res.send('You forgot your parameters')
